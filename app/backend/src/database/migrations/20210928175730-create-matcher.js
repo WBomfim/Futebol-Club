ffb602c22ebbbe2a0126556e50299f7e93e466f0
WBomfim <willianbomfim@hotmail.com>
2022-09-28 18:02:49 -0300

    feat: Criado a migration 20210928175730-create-matcher

---
+++ app/backend/src/database/migrations/20210928175730-create-matcher.js
@@ -0,0 +1,36 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matchers', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      home_team: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      home_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      away_team: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      away_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      in_progress: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('matchers');
+  }
+};
