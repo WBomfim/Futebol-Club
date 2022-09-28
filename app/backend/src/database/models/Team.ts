69ed3bc93df6c4c4a132e7560f45f780bddb70dc
WBomfim <willianbomfim@hotmail.com>
2022-09-28 20:47:48 -0300

    feat: Criado o Model Team

---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,31 @@
+import { INTEGER, STRING, Model } from 'sequelize';
+import db from '.';
+import Match from './Match';
+
+class Team extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'Team',
+  underscored: true,
+  timestamps: false,
+});
+
+Team.hasMany(Match, { foreignKey: 'homeTeam', as: 'matchesHomeTeam' });
+Team.hasMany(Match, { foreignKey: 'awayTeam', as: 'matchesAwayTeam' });
+
+export default Team;
