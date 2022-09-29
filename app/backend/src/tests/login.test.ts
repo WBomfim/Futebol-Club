6f6b6deb699c88580d3d086efc560926dc1d126b
WBomfim <willianbomfim@hotmail.com>
2022-09-29 19:54:57 -0300

    test: Iniciado os testes para a rota /login

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,40 @@
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import * as sinon from 'sinon';
+import { Response } from 'superagent';
+import { app } from '../app';
+import UserModel from '../database/models/User';
+import usersFake from './mocks/Users.json';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testa a rota POST /login', () => {
+  describe('Verifica se o login é realizado com sucesso', () => {
+    let loginResponse: Response;
+    const userResponse = usersFake[0] as UserModel;
+    const userBody = {
+      email: "string",
+      password: "string"
+    }
+
+    before(async () => {
+      sinon.stub(UserModel, "findOne").resolves(userResponse);
+    });
+
+    after(() => {
+      (UserModel.findOne as sinon.SinonStub).restore();
+    });
+
+    it('Retorna um status code "200" e um objeto com a propriedade "token"', async () => {
+      loginResponse = await chai
+        .request(app)
+        .post('/login')
+        .send(userBody);
+      expect(loginResponse).to.have.status(200);
+      expect(loginResponse.body).to.have.property('token');
+    });
+  });
+});
