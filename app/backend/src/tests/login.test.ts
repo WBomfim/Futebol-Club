bacd661f06d3d4f6cbdff0c5fa8f0fa66e9ba470
WBomfim <willianbomfim@hotmail.com>
2022-10-02 19:25:58 -0300

    test: Criado testes para verificar situações invalidas de login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -13,9 +13,10 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Testa a rota POST /login', () => {
+  const userResponse = usersFake[0] as UserModel;
+  let loginResponse: Response;
+
   describe('Verifica se o login é realizado com sucesso', () => {
-    let loginResponse: Response;
-    const userResponse = usersFake[0] as UserModel;
 
     before(() => {
       sinon.stub(UserModel, 'findOne').resolves(userResponse);
@@ -29,6 +30,50 @@ describe('Testa a rota POST /login', () => {
       loginResponse = await chai.request(app).post('/login').send(loginFake.Valid);
       expect(loginResponse).to.have.status(200);
       expect(loginResponse.body).to.have.property('token');
+    });
+  });
+
+  describe('Verifica os erros ao tentar um login com as informações inválidas', () => {
+    it('Retorna um status code "401" e um objeto com a mensagem "Incorrect email or password" ao passar um email inválido', async () => {
+      sinon.stub(UserModel, 'findOne').resolves(null);
+      loginResponse = await chai.request(app).post('/login').send(loginFake.InvalidEmail);
+      expect(loginResponse).to.have.status(401);
+      expect(loginResponse.body).to.have.property('message');
+      expect(loginResponse.body.message).to.be.equal('Incorrect email or password');
+      (UserModel.findOne as sinon.SinonStub).restore();
+    });
+
+    it('Retorna um status code "401" e um objeto com a mensagem "Incorrect email or password" ao passar uma senha inválida', async () => {
+      sinon.stub(UserModel, 'findOne').resolves(userResponse);
+      loginResponse = await chai.request(app).post('/login').send(loginFake.InvalidPassword);
+      expect(loginResponse).to.have.status(401);
+      expect(loginResponse.body).to.have.property('message');
+      expect(loginResponse.body.message).to.be.equal('Incorrect email or password');
+      (UserModel.findOne as sinon.SinonStub).restore();
+    });
+  });
+
+  describe('Verifica os erros ao tentar um login com as informações vazias', () => {
+    before(() => {
+      sinon.stub(UserModel, 'findOne').resolves(userResponse);
+    });
+
+    after(() => {
+      (UserModel.findOne as sinon.SinonStub).restore();
+    });
+
+    it('Retorna um status code "400" e um objeto com a mensagem "All fields must be filled" ao passar um email vazio', async () => {
+      loginResponse = await chai.request(app).post('/login').send(loginFake.emptyEmail);
+      expect(loginResponse).to.have.status(400);
+      expect(loginResponse.body).to.have.property('message');
+      expect(loginResponse.body.message).to.be.equal('All fields must be filled');
+    });
+
+    it('Retorna um status code "400" e um objeto com a mensagem "All fields must be filled" ao passar uma senha vazia', async () => {
+      loginResponse = await chai.request(app).post('/login').send(loginFake.emptyPassword);
+      expect(loginResponse).to.have.status(400);
+      expect(loginResponse.body).to.have.property('message');
+      expect(loginResponse.body.message).to.be.equal('All fields must be filled');
     });
   });
 });
