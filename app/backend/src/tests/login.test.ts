1c7c5792f3206006e7377915cc863a6967034fea
WBomfim <willianbomfim@hotmail.com>
2022-10-03 19:43:28 -0300

    refactor: Refatorado alguns dos testes para o endpoint /login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -14,42 +14,41 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Testa a rota /login', () => {
+  let response: Response;
   const userResponse = usersFake[0] as UserModel;
-  let loginResponse: Response;
 
   describe('Verifica se o login é realizado com sucesso', () => {
-
-    before(() => {
-      sinon.stub(UserModel, 'findOne').resolves(userResponse);
-    });
-
-    after(() => {
-      (UserModel.findOne as sinon.SinonStub).restore();
-    });
-
     it('Retorna um status code "200" e um objeto com a propriedade "token"', async () => {
-      loginResponse = await chai.request(app).post('/login').send(loginFake.Valid);
-      expect(loginResponse).to.have.status(200);
-      expect(loginResponse.body).to.have.property('token');
+      sinon.stub(UserModel, 'findOne').resolves(userResponse);
+
+      response = await chai.request(app).post('/login').send(loginFake.Valid);
+      expect(response).to.have.status(200);
+      expect(response.body).to.have.property('token');
+  
+      (UserModel.findOne as sinon.SinonStub).restore();
     });
   });
 
   describe('Verifica os erros ao tentar um login com as informações inválidas', () => {
     it('Retorna um status code "401" e um objeto com a mensagem "Incorrect email or password" ao passar um email inválido', async () => {
       sinon.stub(UserModel, 'findOne').resolves(null);
-      loginResponse = await chai.request(app).post('/login').send(loginFake.InvalidEmail);
-      expect(loginResponse).to.have.status(401);
-      expect(loginResponse.body).to.have.property('message');
-      expect(loginResponse.body.message).to.be.equal('Incorrect email or password');
+
+      response = await chai.request(app).post('/login').send(loginFake.InvalidEmail);
+      expect(response).to.have.status(401);
+      expect(response.body).to.have.property('message');
+      expect(response.body.message).to.be.equal('Incorrect email or password');
+
       (UserModel.findOne as sinon.SinonStub).restore();
     });
 
     it('Retorna um status code "401" e um objeto com a mensagem "Incorrect email or password" ao passar uma senha inválida', async () => {
       sinon.stub(UserModel, 'findOne').resolves(userResponse);
-      loginResponse = await chai.request(app).post('/login').send(loginFake.InvalidPassword);
-      expect(loginResponse).to.have.status(401);
-      expect(loginResponse.body).to.have.property('message');
-      expect(loginResponse.body.message).to.be.equal('Incorrect email or password');
+
+      response = await chai.request(app).post('/login').send(loginFake.InvalidPassword);
+      expect(response).to.have.status(401);
+      expect(response.body).to.have.property('message');
+      expect(response.body.message).to.be.equal('Incorrect email or password');
+      
       (UserModel.findOne as sinon.SinonStub).restore();
     });
   });
@@ -64,41 +63,35 @@ describe('Testa a rota /login', () => {
     });
 
     it('Retorna um status code "400" e um objeto com a mensagem "All fields must be filled" ao passar um email vazio', async () => {
-      loginResponse = await chai.request(app).post('/login').send(loginFake.emptyEmail);
-      expect(loginResponse).to.have.status(400);
-      expect(loginResponse.body).to.have.property('message');
-      expect(loginResponse.body.message).to.be.equal('All fields must be filled');
+      response = await chai.request(app).post('/login').send(loginFake.emptyEmail);
+      expect(response).to.have.status(400);
+      expect(response.body).to.have.property('message');
+      expect(response.body.message).to.be.equal('All fields must be filled');
     });
 
     it('Retorna um status code "400" e um objeto com a mensagem "All fields must be filled" ao passar uma senha vazia', async () => {
-      loginResponse = await chai.request(app).post('/login').send(loginFake.emptyPassword);
-      expect(loginResponse).to.have.status(400);
-      expect(loginResponse.body).to.have.property('message');
-      expect(loginResponse.body.message).to.be.equal('All fields must be filled');
+      response = await chai.request(app).post('/login').send(loginFake.emptyPassword);
+      expect(response).to.have.status(400);
+      expect(response.body).to.have.property('message');
+      expect(response.body.message).to.be.equal('All fields must be filled');
     });
   });
 
   describe('Verifica o retorno da role do usuário ao acessar a rota /login/validate com o token', () => {
-    let validateResponse: Response;
-
     describe('Verifica o retorno quando o token é valido', () => {
       const payload = { id: 1, username: 'Admin'};
-      
-      before(() => {
-        sinon.stub(jwt, 'verify').resolves(payload);
-        sinon.stub(UserModel, 'findOne').resolves(userResponse);
-      });
-  
-      after(() => {
-        (jwt.verify as sinon.SinonStub).restore();
-        (UserModel.findOne as sinon.SinonStub).restore();
-      });
   
       it('Retorna um status code "200" e um objeto com a propriedade "role"', async () => {
-        validateResponse = await chai.request(app).get('/login/validate').set('Authorization', 'tokentestes');
-        expect(validateResponse).to.have.status(200);
-        expect(validateResponse.body).to.have.property('role');
-        expect(validateResponse.body.role).to.be.equal('admin');
+        sinon.stub(jwt, 'verify').resolves(payload);
+        sinon.stub(UserModel, 'findOne').resolves(userResponse);
+
+        response = await chai.request(app).get('/login/validate').set('Authorization', 'tokentestes');
+        expect(response).to.have.status(200);
+        expect(response.body).to.have.property('role');
+        expect(response.body.role).to.be.equal('admin');
+
+        (jwt.verify as sinon.SinonStub).restore();
+        (UserModel.findOne as sinon.SinonStub).restore();
       });
     });
 
@@ -112,17 +105,17 @@ describe('Testa a rota /login', () => {
       });
 
       it('Retorna um status code "401" com a mensagem "Invalid token"', async () => {
-        validateResponse = await chai.request(app).get('/login/validate').set('Authorization', 'tokentestes');
-        expect(validateResponse).to.have.status(401);
-        expect(validateResponse.body).to.have.property('message');
-        expect(validateResponse.body.message).to.be.equal('Invalid token');
+        response = await chai.request(app).get('/login/validate').set('Authorization', 'tokentestes');
+        expect(response).to.have.status(401);
+        expect(response.body).to.have.property('message');
+        expect(response.body.message).to.be.equal('Invalid token');
       });
 
       it('Retorna um status code "401" com a mensagem "Token not found"', async () => {
-        validateResponse = await chai.request(app).get('/login/validate');
-        expect(validateResponse).to.have.status(401);
-        expect(validateResponse.body).to.have.property('message');
-        expect(validateResponse.body.message).to.be.equal('Token not found');
+        response = await chai.request(app).get('/login/validate');
+        expect(response).to.have.status(401);
+        expect(response.body).to.have.property('message');
+        expect(response.body.message).to.be.equal('Token not found');
       });
     });
   });
