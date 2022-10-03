ed654e331a59590fa076135e2a6f6d6b2988a88c
WBomfim <willianbomfim@hotmail.com>
2022-10-02 21:21:40 -0300

    test: Criado testes para verificar o retorno da rota /login/validate

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -4,6 +4,7 @@ import chaiHttp = require('chai-http');
 import * as sinon from 'sinon';
 import { Response } from 'superagent';
 import { app } from '../app';
+import * as jwt from 'jsonwebtoken';
 import UserModel from '../database/models/User';
 import usersFake from './mocks/users';
 import loginFake from './mocks/logins';
@@ -74,6 +75,55 @@ describe('Testa a rota POST /login', () => {
       expect(loginResponse).to.have.status(400);
       expect(loginResponse.body).to.have.property('message');
       expect(loginResponse.body.message).to.be.equal('All fields must be filled');
+    });
+  });
+
+  describe('Verifica o retorno da role do usuário ao acessar a rota /login/validate com o token', () => {
+    let validateResponse: Response;
+
+    describe('Verifica o retorno quando o token é valido', () => {
+      const payload = { id: 1, username: 'Admin'};
+      
+      before(() => {
+        sinon.stub(jwt, 'verify').resolves(payload);
+        sinon.stub(UserModel, 'findOne').resolves(userResponse);
+      });
+  
+      after(() => {
+        (jwt.verify as sinon.SinonStub).restore();
+        (UserModel.findOne as sinon.SinonStub).restore();
+      });
+  
+      it('Retorna um status code "200" e um objeto com a propriedade "role"', async () => {
+        validateResponse = await chai.request(app).get('/login/validate').set('Authorization', 'tokentestes');
+        expect(validateResponse).to.have.status(200);
+        expect(validateResponse.body).to.have.property('role');
+        expect(validateResponse.body.role).to.be.equal('admin');
+      });
+    });
+
+    describe('Verifica o retorno quando o token é invalido ou inexistente', () => {
+      before(() => {
+        sinon.stub(jwt, 'verify').throws();
+      });
+
+      after(() => {
+        (jwt.verify as sinon.SinonStub).restore();
+      });
+
+      it('Retorna um status code "401" com a mensagem "Invalid token"', async () => {
+        validateResponse = await chai.request(app).get('/login/validate').set('Authorization', 'tokentestes');
+        expect(validateResponse).to.have.status(401);
+        expect(validateResponse.body).to.have.property('message');
+        expect(validateResponse.body.message).to.be.equal('Invalid token');
+      });
+
+      it('Retorna um status code "401" com a mensagem "Token not found"', async () => {
+        validateResponse = await chai.request(app).get('/login/validate');
+        expect(validateResponse).to.have.status(401);
+        expect(validateResponse.body).to.have.property('message');
+        expect(validateResponse.body.message).to.be.equal('Token not found');
+      });
     });
   });
 });
