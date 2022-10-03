89ce9247640baffbe6a0ef425038f04c4f7f1f65
WBomfim <willianbomfim@hotmail.com>
2022-10-03 18:31:11 -0300

    feat: Criado a classe TeamsCotroller

---
+++ app/backend/src/controllers/TeamsController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import TeamsService from '../services/TeamsService';
+
+export default class TeamsController {
+  private _teamsService: TeamsService;
+
+  constructor() {
+    this._teamsService = new TeamsService();
+  }
+
+  public async getTeams(req: Request, res: Response): Promise<Response> {
+    const { code, data, error } = await this._teamsService.getTeams();
+    return res.status(code).json(data || error);
+  }
+
+  public async getTeam(req: Request, res: Response): Promise<Response> {
+    const { code, data, error } = await this._teamsService.getTeam(Number(req.params.id));
+    return res.status(code).json(data || error);
+  }
+}
