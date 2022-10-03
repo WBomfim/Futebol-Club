83c5dd0c674e7d3e99683446dc3a3a9c4ba930c5
WBomfim <willianbomfim@hotmail.com>
2022-10-03 19:37:38 -0300

    test: Criado testes para o endpoint GET /teams/:id

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -11,26 +11,44 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Testa as rotas do endpoint /teams', () => {
+  let response: Response
+  
   describe('Verifica o retorno ao acessar a rota /teams', () => {
-    let responseTeams: Response
 
     it('Retorna um status code "200" e um array de objetos contendo os times', async () => {
       Sinon.stub(Team, 'findAll').resolves(teamsFake as Team[]);
-      responseTeams = await chai.request(app).get('/teams');
-      expect(responseTeams).to.have.status(200);
-      expect(responseTeams.body).to.deep.equal(teamsFake);
+      response = await chai.request(app).get('/teams');
+      expect(response).to.have.status(200);
+      expect(response.body).to.deep.equal(teamsFake);
       (Team.findAll as Sinon.SinonStub).restore();
     });
 
     it('Retorna um status code "404" e um objeto com a mensagem "Teams not found" quando não existem times cadastrados', async () => {
       Sinon.stub(Team, 'findAll').resolves([]);
-      responseTeams = await chai.request(app).get('/teams');
-      expect(responseTeams).to.have.status(404);
-      expect(responseTeams.body).to.have.property('message');
-      expect(responseTeams.body.message).to.be.equal('Teams not found');
+      response = await chai.request(app).get('/teams');
+      expect(response).to.have.status(404);
+      expect(response.body).to.have.property('message');
+      expect(response.body.message).to.be.equal('Teams not found');
       (Team.findAll as Sinon.SinonStub).restore();
     });
   });
 
-  describe('Verifica o retorno ao acessar a rota /teams/1', () => {});
+  describe('Verifica o retorno ao acessar a rota /teams/1', () => {
+    it('Retorna um status code "200" e um objeto contendo o time', async () => {
+      Sinon.stub(Team, 'findOne').resolves(teamsFake[0] as Team);
+      response = await chai.request(app).get('/teams/1');
+      expect(response).to.have.status(200);
+      expect(response.body).to.deep.equal(teamsFake[0]);
+      (Team.findOne as Sinon.SinonStub).restore();
+    });
+
+    it('Retorna um status code "404" e um objeto com a mensagem "Team not found" quando o time não existe', async () => {
+      Sinon.stub(Team, 'findOne').resolves(null);
+      response = await chai.request(app).get('/teams/1');
+      expect(response).to.have.status(404);
+      expect(response.body).to.have.property('message');
+      expect(response.body.message).to.be.equal('Team not found');
+      (Team.findOne as Sinon.SinonStub).restore();
+    });
+  });
 });
