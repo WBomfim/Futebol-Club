226b2baacfc47369c2de7587d207ced7f7b813ba
WBomfim <willianbomfim@hotmail.com>
2022-10-04 00:59:14 -0300

    chore: Inserido a autorização via token na rota POST /matches

--- app/backend/src/routes/MatchesRoutes.ts
+++ app/backend/src/routes/MatchesRoutes.ts
@@ -1,18 +1,25 @@
 import { Router } from 'express';
+import Token from '../helpers/Token';
 import MatchesController from '../controllers/MatchesController';
 
 export default class MatchesRoutes {
   private _router: Router;
+  private _token: Token;
   private _matchesController: MatchesController;
 
   constructor() {
     this._router = Router();
+    this._token = new Token();
     this._matchesController = new MatchesController();
   }
 
   public routes(): Router {
     this._router.get('/', (req, res) => this._matchesController.getMatches(req, res));
-    this._router.post('/', (req, res) => this._matchesController.createMatch(req, res));
+    this._router.post(
+      '/',
+      (req, res, next) => this._token.verifyToken(req, res, next),
+      (req, res) => this._matchesController.createMatch(req, res),
+    );
 
     return this._router;
   }
