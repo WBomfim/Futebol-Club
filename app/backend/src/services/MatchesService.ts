00a29d0cb95ab43b182dc51afe26d94e125b6d3f
WBomfim <willianbomfim@hotmail.com>
2022-10-04 02:05:09 -0300

    feat: Inserido as verificações criadas no metodo createMatch da classe MatchesService

--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -1,14 +1,18 @@
 import Match from '../database/models/Match';
 import Team from '../database/models/Team';
+import validateMatch from '../schemas/ValidateInfosMatch';
 import { ReturnMatch, MatchIncludesTeams } from '../interfaces/ReturnService';
 import { MatchCreate } from '../interfaces/BodyRequest';
+import ReturnError from '../interfaces/ReturnError';
 
 export default class MatchesService {
   private _matches: typeof Match;
+  private _validateMatch: typeof validateMatch;
   private _associations: object[];
 
   constructor() {
     this._matches = Match;
+    this._validateMatch = validateMatch;
     this._associations = [
       { model: Team, as: 'teamHome', attributes: ['teamName'] },
       { model: Team, as: 'teamAway', attributes: ['teamName'] },
@@ -32,6 +36,9 @@ export default class MatchesService {
   }
 
   public async createMatch(match: MatchCreate): Promise<ReturnMatch> {
+    const { error } = await this._validateMatch.validateInfosMatch(match) as ReturnError;
+    if (error) return error;
+
     const data = await this._matches.create(match);
     return { code: 201, data };
   }
