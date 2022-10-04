21edecb89b09ec563401a9dceed7de0442bc0d32
WBomfim <willianbomfim@hotmail.com>
2022-10-04 16:41:08 -0300

    refactor: Separado as interfaces de Matches para utilizarmos apenas o necessário em cada metodo

--- app/backend/src/interfaces/BodyRequest.ts
+++ app/backend/src/interfaces/BodyRequest.ts
@@ -1,7 +1,10 @@
-export interface MatchCreate {
-  homeTeam: number;
-  awayTeam: number;
+export interface MatchUpdate {
   homeTeamGoals: number;
   awayTeamGoals: number;
+}
+
+export interface MatchCreate extends MatchUpdate {
+  homeTeam: number;
+  awayTeam: number;
   inProgress: boolean;
 }
