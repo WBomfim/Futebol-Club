7964d7bfccba29457747be41315418a51644c5da
WBomfim <willianbomfim@hotmail.com>
2022-10-04 02:03:27 -0300

    feat: Criado a classe ValidateInfosMatch para validar os dados de uma partida.

---
+++ app/backend/src/schemas/ValidateInfosMatch.ts
@@ -0,0 +1,31 @@
+import { Op } from 'sequelize';
+import { MatchCreate } from '../interfaces/BodyRequest';
+import ReturnError from '../interfaces/ReturnError';
+import Team from '../database/models/Team';
+import StatusHttp from '../types/statusHttp';
+
+export default class validateInfosMatch extends Team {
+  public static async validateInfosMatch(match: MatchCreate): Promise<ReturnError | false> {
+    const { homeTeam, awayTeam } = match;
+
+    if (homeTeam === awayTeam) {
+      return { error: {
+        code: StatusHttp.UNAUTHORIZED,
+        error: { message: 'It is not possible to create a match with two equal teams' } },
+      };
+    }
+
+    const isTeams = await this.findAll({
+      where: { [Op.or]: [{ id: homeTeam }, { id: awayTeam }] },
+    });
+
+    if (isTeams.length !== 2) {
+      return { error: {
+        code: StatusHttp.NOT_FOUND,
+        error: { message: 'There is no team with such id!' } },
+      };
+    }
+
+    return false;
+  }
+}
