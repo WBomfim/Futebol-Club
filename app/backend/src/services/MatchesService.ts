f568ccaf1f49079c064d9511e22147690b7e33bd
WBomfim <willianbomfim@hotmail.com>
2022-10-03 22:54:52 -0300

    feat: Criado um metodo na classe MatchersService para filtrar a busca pelo progresso da partida

--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -4,17 +4,27 @@ import { ReturnMatch } from '../interfaces/ReturnService';
 
 export default class MatchesService {
   private _matches: typeof Match;
+  private _associations: object[];
 
   constructor() {
     this._matches = Match;
-  }
-
-  public async getMatches(): Promise<ReturnMatch> {
-    const ASSOCIATIONS = [
+    this._associations = [
       { model: Team, as: 'teamHome', attributes: ['teamName'] },
       { model: Team, as: 'teamAway', attributes: ['teamName'] },
     ];
-    const data = await this._matches.findAll({ include: ASSOCIATIONS });
+  }
+
+  public async getMatches(): Promise<ReturnMatch> {
+    const data = await this._matches.findAll({ include: this._associations });
+
+    if (!data || data.length === 0) return { code: 404, error: { message: 'No matches found' } };
+    return { code: 200, data };
+  }
+
+  public async getProgressMatches(inProgress: boolean): Promise<ReturnMatch> {
+    const data = await this._matches.findAll({
+      where: { inProgress }, include: this._associations,
+    });
 
     if (!data || data.length === 0) return { code: 404, error: { message: 'No matches found' } };
     return { code: 200, data };
