5a5f0b34844968d27cf88bcb668338a0b24d5dd4
WBomfim <willianbomfim@hotmail.com>
2022-10-03 21:24:30 -0300

    feat: Adequando o metodo getMatches da classe MatchesService para retornar os nomes dos times associados as foreignKey

--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -1,4 +1,5 @@
 import Match from '../database/models/Match';
+import Team from '../database/models/Team';
 import { ReturnMatch } from '../interfaces/ReturnService';
 
 export default class MatchesService {
@@ -9,7 +10,12 @@ export default class MatchesService {
   }
 
   public async getMatches(): Promise<ReturnMatch> {
-    const data = await this._matches.findAll();
+    const ASSOCIATIONS = [
+      { model: Team, as: 'teamHome', attributes: ['teamName'] },
+      { model: Team, as: 'teamAway', attributes: ['teamName'] },
+    ];
+    const data = await this._matches.findAll({ include: ASSOCIATIONS });
+
     if (!data || data.length === 0) return { code: 404, error: { message: 'No matches found' } };
     return { code: 200, data };
   }
